372d04af264d94d9959ebe282c8e156fa4b0b426
DariaHalchenko <154507353+DariaHalchenko@users.noreply.github.com>
2024-02-14 11:57:43 +0200

    Update praktiline_iseseisevtöö_Listid.py

--- praktiline_iseseisevtöö_Listid.py
+++ praktiline_iseseisevtöö_Listid.py
@@ -1,4 +1,4 @@
-#1 Sõna/Lause
+#1.  Sõna/Lause
 #from string import punctuation
 
 #vokaali=["a","o","e","i","u","õ","ä","ö","ü","y"]
@@ -21,35 +21,109 @@
 #print("Kirjuvahemärgid: ",m) 
 #print("Tühikud: ",t)
 
-#2 Loetelu
-nimed=[] 
-for i in range(5):
-    nimi=input("Sisesta nime: ").capitalize()
-    nimed.append(nimi)
-nimed.sort()
-print("Nimed tähestikulises järjekorras: "+str(nimed))
-print("Viimati lisatud nimi: "+str(nimed[-1]))
-index=int(input("Sisestage nimi, mida soovite kustutada: ")) 
-new_nimi=input("Sisesta nimi: ")
-nimed[index]=new_nimi 
-print("Nimede loetelu: "+str(nimed)) 
-uued_nimed=[] 
-for nimi in nimed:
-    if nimi not in uued_nimed:
-        uued_nimed.append(nimi)
-print("Loetelu ilma identsete nimedeta: ",uued_nimed)
+#2.  Loetelu
+#nimed=[] 
+#for i in range(5):
+#    nimi=input("Sisesta nime: ").capitalize()
+#    nimed.append(nimi)
+#nimed.sort()
+#print("Nimed tähestikulises järjekorras: "+str(nimed))
+#print("Viimati lisatud nimi: "+str(nimed[-1]))
+#index=int(input("Sisestage nimi, mida soovite kustutada: ")) 
+#new_nimi=input("Sisesta nimi: ")
+#nimed[index]=new_nimi 
+#print("Nimede loetelu: "+str(nimed)) 
+#uued_nimed=[] 
+#for nimi in nimed:
+#    if nimi not in uued_nimed:
+#        uued_nimed.append(nimi)
+#print("Loetelu ilma identsete nimedeta: ",uued_nimed)
 
-vanus=[]
-for i in range(5):
-    aastat=input("Sisestage oma vanus: ").capitalize()
-    vanus.append(aastat) 
-vanus.sort() 
-print("Vanuste loend: "+str(vanus)) 
-max_age=max(vanus) 
-min_age=min(vanus) 
-summa_age=sum(vanus) 
-average_age=sum(vanus)/len(vanus)
-print(max_age)
-print(min_age) 
-print(summa_age) 
-print(average_age)
+#vanused=[]
+#for i in range(5):
+#    vanus=int(input("Sisestage oma vanus: "))
+#    vanused.append(vanus) 
+#vanused.sort() 
+#print("Vanuste loend: "+str(vanused)) 
+#maksimum=max(vanused) 
+#minimum=min(vanused) 
+#summa=sum(vanused) 
+#keskmine=summa/len(vanused)
+#print("Maksimum: "+str(maksimum), "Minimum: "+str(minimum), "Summa: "+str(summa), "Keskmine: "+str(keskmine))
+#for i in range(5):
+#    print(nimed[i], "on" , vanused[i], "aastat vana")
+
+
+#3. Tärnid
+#from random import *
+#arvud=[] 
+#N=int(input("Mitu rida joonistame? ")) 
+#S=input("Sisesta sümbol: ") 
+##loendi täitmine
+#for p in range (N):
+#    arvud.append(randint(1,100))
+##diagrammi loomine
+#for p in range(N):
+#    print(arvud[p]*S) 
+
+
+#4. Postiindex
+#Indeksid=["Tallinn", "Narva, Narva-Jõesuu", "Kohtla-Järve","Ida-Virumaa, Lääne-Virumaa, Jõgevamaa","Tartu linn",
+#          "Tartumaa, Põlvamaa, Võrumaa, Valgamaa", "Viljandimaa, Järvamaa, Harjumaa, Raplamaa", "Pärnumaa",
+#          "Läänemaa, Hiiumaa, Saaremaa"] 
+#while True:
+#    while True:
+#        try:
+#            indeks=int(input("Sisesta viienubriline indeks: ")) 
+#            #10000,15478,98854
+#            #if 10000<=indeks<=99999
+#            indeks_elemendide_arv=len(str(indeks)) 
+#            if indeks_elemendide_arv==5:
+#                print("5 numbriline indeks:  ") 
+#                break
+#            else:
+#                print("On vaja 5 numbriline arv (indeks)") 
+#        except:
+#            print("Vale andmetüüp!")
+#        arv1=indeks//10000
+#        print(arv1)       
+#        symbolid=list(str(indeks)) 
+#        print(f"Sa elad piirkonnas {Indeksid[int(symbolid[0])-1]}") 
+
+
+#5. Vahetus
+#from random import *
+#from string import *
+#nimekirja=[] 
+#N=randint(2,35) 
+#for i in range(N):
+#    nimekirja.append(choice(ascii_uppercase)) 
+#print(nimekirja) 
+#kogus=int(input("Mitu elemendi vahetame oma vahel: "))
+#if len(nimekirja)//2>=kogus:
+#    for i in range(kogus):
+#        a=nimekirja[i] 
+#        nimekirja[i]=nimekirja[len(nimekirja)-i-1] 
+#        nimekirja[len(nimekirja)-1-i]=a 
+#print(nimekirja)
+
+#6. Бесполезные числа
+from random import *
+nimekirja1=[]
+nimekirja=[] 
+N=int(input("Sisestage numbrite pikkus: "))
+for i in range(N):
+    arv=randint(10,100)
+    nimekirja1.append(arv)
+    nimekirja.append(arv)
+maksimum=nimekirja[0]
+for arv in nimekirja:
+    if arv>maksimum:
+        maksimum=arv
+        useless_number=maksimum/len(nimekirja) 
+for i in range(len(nimekirja)):
+    if nimekirja[i]==maksimum:
+        nimekirja[i]=useless_number
+print("Esialgne nimekiri: "+str(nimekirja1))
+print("Muudetud numbriga loend: "+str(nimekirja))
+
